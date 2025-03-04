707b671cc7422e7328538089f3821edefbcb3e4b
Loong <wentloop@gmail.com>
2025-03-04 17:44:02 +0800

    style: 调整横幅渐变色和按钮颜色，更新导航项链接
    - 调整横幅组件的渐变色和按钮颜色以提升视觉效果。
    - 为导航项添加链接并实现点击跳转功能。

--- apps/web-hoby/src/views/_core/home/layout/Header.vue
+++ apps/web-hoby/src/views/_core/home/layout/Header.vue
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-02-27 16:17:55
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-04 12:13:17
+ * @LastEditTime: 2025-03-04 15:41:10
  * @FilePath: \HOBY-platform\app\components\layout\Header.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
