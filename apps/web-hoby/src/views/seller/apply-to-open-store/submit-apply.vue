5017bc92def1e65b5890137dc983097062ebd62c
Loong <wentloop@gmail.com>
2025-03-10 14:47:56 +0800

    feat: 添加直销和自营店铺相关页面及路由

---
+++ apps/web-hoby/src/views/seller/apply-to-open-store/submit-apply.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: Loong wentloop@gmail.com
+ * @Date: 2025-03-10 14:35:25
+ * @LastEditors: Loong wentloop@gmail.com
+ * @LastEditTime: 2025-03-10 14:45:47
+ * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\seller\apply-to-open-store\submit-apply.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup lang="ts">
+import { Page } from '@vben/common-ui';
+</script>
+
+<template>
+  <Page>
+    <div class="p-6">
+      <h1 class="mb-6 text-lg font-medium">提交开店申请</h1>
+      <!-- 店铺内容 -->
+      <div class="rounded-lg bg-white p-6 shadow-sm">
+        <div class="text-center text-gray-500">开店申请功能开发中...</div>
+      </div>
+    </div>
+  </Page>
+</template>
