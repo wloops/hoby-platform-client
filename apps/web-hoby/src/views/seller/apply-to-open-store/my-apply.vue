334a479fa917d25277ea210d1e63e2ac844a73c4
Loong <wentloop@gmail.com>
2025-03-10 14:47:56 +0800

    feat: 添加直销和自营店铺相关页面及路由

--- apps/web-hoby/src/views/seller/apply-to-open-store/my-apply.vue
+++ apps/web-hoby/src/views/seller/apply-to-open-store/my-apply.vue
@@ -1,6 +1,3 @@
-<!--
- * @Description: 店铺管理
--->
 <script setup lang="ts">
 import { Page } from '@vben/common-ui';
 </script>
@@ -8,10 +5,10 @@ import { Page } from '@vben/common-ui';
 <template>
   <Page>
     <div class="p-6">
-      <h1 class="mb-6 text-lg font-medium">我的店铺</h1>
+      <h1 class="mb-6 text-lg font-medium">我的开店申请</h1>
       <!-- 店铺内容 -->
       <div class="rounded-lg bg-white p-6 shadow-sm">
-        <div class="text-center text-gray-500">店铺管理功能开发中...</div>
+        <div class="text-center text-gray-500">开店申请功能开发中...</div>
       </div>
     </div>
   </Page>
---
+++ apps/web-hoby/src/views/seller/self-sales/select-goods.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: Loong wentloop@gmail.com
+ * @Date: 2025-03-09 21:30:35
+ * @LastEditors: Loong wentloop@gmail.com
+ * @LastEditTime: 2025-03-10 13:39:15
+ * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\seller\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<!--
+ * @Description: 店铺管理
+-->
+<script setup lang="ts">
+import { Page } from '@vben/common-ui';
+</script>
+
+<template>
+  <Page>
+    <div class="p-6">
+      <h1 class="mb-6 text-lg font-medium">我的店铺</h1>
+      <!-- 店铺内容 -->
+      <div class="rounded-lg bg-white p-6 shadow-sm">
+        <div class="text-center text-gray-500">店铺管理功能开发中...</div>
+      </div>
+    </div>
+  </Page>
+</template>
