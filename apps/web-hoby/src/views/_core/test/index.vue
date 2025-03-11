73019d74be1768ed623989a3a0e19dc927571dde
Loong <wentloop@gmail.com>
2025-03-11 18:11:05 +0800

    feat: 添加通用数据获取接口
    - 添加通用数据获取接口以支持通过API请求获取数据。

--- apps/web-hoby/src/views/_core/test/index.vue
+++ apps/web-hoby/src/views/_core/test/index.vue
@@ -2,19 +2,23 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-03 21:58:41
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-11 10:58:23
+ * @LastEditTime: 2025-03-11 17:59:11
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\_core\test\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <script lang="ts" setup>
 import { Button } from 'ant-design-vue';
 
-import { getUserInfoApi } from '#/api/core/user';
+import { getCommonData } from '#/api/core/main';
 
 defineOptions({ name: 'Test' });
-
+const data = {
+  pageID: 'mySaleOrderForRestockingPage',
+  pageDataGrpID: 'mySaleOrderForRestocking',
+  TELLERCOMPANY: '测试仓商',
+};
 const handleClick = () => {
-  getUserInfoApi().then((res) => {
+  getCommonData(data).then((res) => {
     console.warn(res);
   });
 };
