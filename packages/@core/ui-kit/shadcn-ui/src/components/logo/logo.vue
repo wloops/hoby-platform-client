cfbe1daa62736ad24e390432c5daf747401e37e5
Loong <wentloop@gmail.com>
2025-03-17 13:20:16 +0800

    refactor: 移除注释并恢复默认 `href` 值
    - 移除文件头部注释并恢复默认的 `href` 值。
    - 添加点击Logo跳转至首页的功能。

--- packages/@core/ui-kit/shadcn-ui/src/components/logo/logo.vue
+++ packages/@core/ui-kit/shadcn-ui/src/components/logo/logo.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: Loong wentloop@gmail.com
- * @Date: 2025-03-03 21:58:41
- * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-10 11:03:18
- * @FilePath: \hoby-platform-client\packages\@core\ui-kit\shadcn-ui\src\components\logo\logo.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <script setup lang="ts">
 import { VbenAvatar } from '../avatar';
 
@@ -42,8 +34,7 @@ defineOptions({
 
 withDefaults(defineProps<Props>(), {
   collapsed: false,
-  // href: 'javascript:void 0',
-  href: import.meta.env.VITE_HOME_PATH,
+  href: 'javascript:void 0',
   logoSize: 32,
   src: '',
   theme: 'light',
