ec46ba1ddbc8eb3e8dea3c8cb5d6d9f111ea5df8
Loong <wentloop@gmail.com>
2025-03-18 17:06:49 +0800

    refactor: 更新买家路由配置添加活动路径
    - 更新买家路由配置以添加活动路径。

--- apps/web-hoby/src/router/routes/modules/buyer.ts
+++ apps/web-hoby/src/router/routes/modules/buyer.ts
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-04 16:17:01
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-17 13:45:56
+ * @LastEditTime: 2025-03-18 16:39:47
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\router\routes\modules\buyer.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -94,6 +94,7 @@ const routes: RouteRecordRaw[] = [
           title: $t('page.buyer.purchaseGoods.orderProduct'),
           authority: ['buyer'],
           hideInMenu: true,
+          activePath: '/buyer/purchaseGoods',
         },
         name: 'BuyerOrderProduct',
         path: '/buyer/orderProduct',
