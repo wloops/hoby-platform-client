a276221899f0bba7ebef2928867e581bb886313e
Loong <wentloop@gmail.com>
2025-03-18 10:29:55 +0800

    feat: 新增仓库管理模块、创建各级缺省页面

---
+++ apps/web-hoby/src/views/warehouse/private/management/storage-sheet.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Author: Loong wentloop@gmail.com
+ * @Date: 2025-03-17 17:01:04
+ * @LastEditors: Loong wentloop@gmail.com
+ * @LastEditTime: 2025-03-17 17:03:50
+ * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\warehouse\private\management\sku.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script lang="ts" setup></script>
+
+<template>
+  <div class="h-full p-4">
+    <div class="mb-4">
+      <h1 class="text-lg font-bold">
+        {{ $t('page.warehouse.myPrivateWarehouse.management.warehouseBatch') }}
+      </h1>
+    </div>
+  </div>
+</template>
