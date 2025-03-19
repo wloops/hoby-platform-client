8d27b0009fc7eebdbd27032c767865c099b6ebaa
Loong <wentloop@gmail.com>
2025-03-19 17:09:03 +0800

    style: 优化可编辑单元格图标样式与交互
    - 优化可编辑单元格的图标样式和交互行为。

--- apps/web-hoby/src/components/DataTable/components/EditableCell.vue
+++ apps/web-hoby/src/components/DataTable/components/EditableCell.vue
@@ -177,20 +177,24 @@ const save = () => {
         <div v-if="help" class="error-message">{{ help }}</div>
       </FormItem>
       <svg
-        class="ml-2 text-green-500"
+        @click="save"
+        class="editable-icon ml-2 cursor-pointer text-xs text-green-500"
         xmlns="http://www.w3.org/2000/svg"
         width="20"
         height="20"
         viewBox="0 0 24 24"
-        @click="save"
       >
         <!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ -->
-        <path
-          fill="currentColor"
-          fill-rule="evenodd"
-          d="M12 22c-4.714 0-7.071 0-8.536-1.465C2 19.072 2 16.714 2 12s0-7.071 1.464-8.536C4.93 2 7.286 2 12 2s7.071 0 8.535 1.464C22 4.93 22 7.286 22 12s0 7.071-1.465 8.535C19.072 22 16.714 22 12 22m4.03-13.03a.75.75 0 0 1 0 1.06l-5 5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 1 1 1.06-1.06l1.47 1.47l4.47-4.47a.75.75 0 0 1 1.06 0"
-          clip-rule="evenodd"
-        />
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1.5"
+        >
+          <path d="m4 12.9l3.143 3.6L15 7.5" opacity=".5" />
+          <path d="m20 7.563l-8.571 9L11 16" />
+        </g>
       </svg>
     </div>
     <div v-else class="editable-cell-text-wrapper">
@@ -199,7 +203,7 @@ const save = () => {
         <!-- <EditOutlined  class="editable-icon" /> -->
         <svg
           v-if="editable"
-          class="editable-icon"
+          class="editable-icon text-blue-500"
           xmlns="http://www.w3.org/2000/svg"
           width="15"
           height="15"
@@ -256,7 +260,6 @@ const save = () => {
 .editable-icon {
   flex-shrink: 0;
   font-size: 14px;
-  color: #1890ff;
 }
 
 .editable-cell-trigger:hover {
