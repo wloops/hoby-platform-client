517b095e79d192c2f4c25f949854c38ba1fbaba5
Loong <wentloop@gmail.com>
2025-03-21 16:13:58 +0800

    feat: 添加 `useServiceCall` 可组合函数
    - 添加新的可组合函数 `useServiceCall` 并更新文件头部注释信息。
    - 创建一个服务调用钩子以管理异步操作的状态和批量处理逻辑。

--- apps/web-hoby/src/composables/index.ts
+++ apps/web-hoby/src/composables/index.ts
@@ -1,5 +1,14 @@
+/*
+ * @Author: Loong wentloop@gmail.com
+ * @Date: 2025-03-18 11:26:16
+ * @LastEditors: Loong wentloop@gmail.com
+ * @LastEditTime: 2025-03-21 16:12:43
+ * @FilePath: \hoby-platform-client\apps\web-hoby\src\composables\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 export * from './encrypt/encryption';
 export * from './useDynamicRoutes';
 export * from './useEnums';
 export * from './useMainGetData';
 export * from './useParseResponse';
+export * from './useServiceCall';
