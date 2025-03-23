9353eab3bea93e7690c78f445dd475c471031eea
Loong <wentloop@gmail.com>
2025-03-23 15:10:52 +0800

    feat: 添加用户导航功能及相关页面
    - 添加用户导航功能并优化布局组件依赖项。
    - 添加用户页面路由到核心路由配置中。
    - 添加用户认证页面的基础模板。
    - 在头部组件中添加用户菜单项并实现跳转功能。

---
+++ apps/web-hoby/src/views/_core/authentication/user.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author: Loong wentloop@gmail.com
+ * @Date: 2025-03-21 18:26:04
+ * @LastEditors: Loong wentloop@gmail.com
+ * @LastEditTime: 2025-03-21 18:26:18
+ * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\_core\authentication\user.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <h1>我的</h1>
+  </div>
+</template>
