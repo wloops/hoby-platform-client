b9778c3d6b234b5e2161158d11cf704e4d086941
Loong <wentloop@gmail.com>
2025-03-25 14:10:04 +0800

    feat: 添加级联数据请求接口及处理逻辑
    - 添加通用级联获取数据接口以支持级联数据请求。
    - 添加级联数据请求处理逻辑并优化数据获取函数。

--- apps/web-hoby/src/api/core/main.ts
+++ apps/web-hoby/src/api/core/main.ts
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-03 21:58:41
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-14 10:46:58
+ * @LastEditTime: 2025-03-25 13:49:00
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\api\core\main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,6 +18,14 @@ export async function mainGetDataApi(data: any) {
   });
 }
 
+/**
+ * 通用级联获取数据接口
+ */
+export async function mainGetDataApiCascade(data: any) {
+  return requestClient.post<any>('/datasetService', data, {
+    withCredentials: true,
+  });
+}
 /**
  * 通用服务接口
  */
--- apps/web-hoby/src/composables/useMainGetData.ts
+++ apps/web-hoby/src/composables/useMainGetData.ts
@@ -2,13 +2,13 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-12 12:01:24
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-14 14:12:34
+ * @LastEditTime: 2025-03-25 13:52:43
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\composables\useMainGetData.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { ref } from 'vue';
 
-import { mainGetDataApi } from '#/api';
+import { mainGetDataApi, mainGetDataApiCascade } from '#/api';
 
 /**
  * 通用的数据获取 Hook
@@ -23,25 +23,45 @@ export const useMainGetData = async <T>(
   const loading = ref(true); // 加载状态
   const error = ref<Error | null>(null); // 错误信息
 
+  const params = ref<Record<string, any>>({
+    ...reqParams,
+  });
+  const isCascade = ref(false);
+  // 处理cascade数据集请求参数
+  if (reqParams.pageID === '' && reqParams.pageDataGrpID) {
+    params.value.bllID = reqParams.pageDataGrpID;
+    delete params.value.pageID;
+    delete params.value.pageDataGrpID;
+    isCascade.value = true;
+  }
   // 数据获取函数
   const fetchData = async () => {
     try {
       loading.value = true; // 开始加载
       error.value = null; // 清空错误信息
-      const response = await mainGetDataApi(reqParams); // 调用 API
+      if (isCascade.value === false) {
+        const response = await mainGetDataApi(params.value); // 调用 API
+        const code = response.rs;
+        // 如果code不是1，则抛出错误
+        if (code !== '1') {
+          throw new Error(`API returned code: ${code}`);
+        }
+        const result = response[reqParams.pageDataGrpID]; // 动态获取需要的字段
+        const responseTotal =
+          response[`${reqParams.pageDataGrpID}_totalRecNum`]?.[0]?.TOTALRECNUM;
+        data.value = result; // 设置数据
+        total.value = Number(responseTotal);
+      } else {
+        const response = await mainGetDataApiCascade(params.value); // 调用 API
 
-      const code = response.rs;
-      // 如果code不是1，则抛出错误
-      if (code !== '1') {
-        throw new Error(`API returned code: ${code}`);
+        const code = response.rs;
+        // 如果code不是1，则抛出错误
+        if (code !== '1') {
+          throw new Error(`API returned code: ${code}`);
+        }
+        data.value = getCascadeData(response); // 设置数据
+        total.value = 1;
       }
-
-      const result = response[reqParams.pageDataGrpID]; // 动态获取需要的字段
-      const responseTotal =
-        response[`${reqParams.pageDataGrpID}_totalRecNum`]?.[0]?.TOTALRECNUM;
-
-      data.value = result; // 设置数据
-      total.value = Number(responseTotal);
     } catch (error_) {
       console.error('Error fetching data:', error_); // 打印错误信息
       error.value = error_ as Error; // 捕获错误
@@ -62,4 +82,17 @@ export const useMainGetData = async <T>(
     error,
     refresh: fetchData, // 提供刷新方法
   };
+};
+
+const getCascadeData = async (data: any) => {
+  // 获取data对象的key,只有一个且非 rs
+  const keys = Object.keys(data).filter((key) => key !== 'rs');
+  if (keys.length === 1) {
+    const key = keys[0] as string;
+    const response = data[key][0];
+    if (response) {
+      return response;
+    }
+  }
+  return null;
 };
