ef60a190a08282edfdc3a16a4d8aa56ebc031582
Loong <wentloop@gmail.com>
2025-03-27 18:54:32 +0800

    feat: 动态表单组件添加页面ID属性
    - 为动态表单组件添加页面ID属性以增强功能。

--- apps/web-hoby/src/views/warehouse/private/general-ledger/open.vue
+++ apps/web-hoby/src/views/warehouse/private/general-ledger/open.vue
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-18 11:26:16
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-26 11:59:06
+ * @LastEditTime: 2025-03-27 18:43:50
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\warehouse\private\management\type.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -28,6 +28,8 @@ import { useMainGetData } from '#/composables';
 const pageTitle = $t(
   'page.warehouse.myPrivateWarehouse.generalLedger.openWarehouse',
 );
+const pageID = 'hobySelPrivcateWareSrlDir';
+
 const dynamicFormRef = ref<null | {
   open: () => void;
 }>(null);
@@ -199,7 +201,11 @@ defineExpose({
       @page-change="handlePageChange"
       @selection-change="handleSelectionChange"
     />
-    <DynamicForm ref="dynamicFormRef" :title="dynamicFormTitle" />
+    <DynamicForm
+      ref="dynamicFormRef"
+      :title="dynamicFormTitle"
+      :page-id="pageID"
+    />
   </Page>
 </template>
 
