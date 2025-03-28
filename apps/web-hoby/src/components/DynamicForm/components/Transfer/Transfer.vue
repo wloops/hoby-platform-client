c506853255e08b61b141402fc29ad72f813bace1
Loong <wentloop@gmail.com>
2025-03-28 22:36:18 +0800

    refactor: 重构Transfer组件以支持外部数据源
    - 重构Transfer组件以移除模拟数据并接收外部数据源作为props。
    - 重构动态表单中的Transfer组件以支持从外部API获取数据并优化模态框的打开逻辑。

--- apps/web-hoby/src/components/DynamicForm/components/Transfer/Transfer.vue
+++ apps/web-hoby/src/components/DynamicForm/components/Transfer/Transfer.vue
@@ -2,27 +2,38 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-26 13:24:12
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-26 16:05:51
+ * @LastEditTime: 2025-03-28 22:36:51
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\components\DynamicForm\components\Transfer\Transfer.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
 import { useVbenModal } from '@vben/common-ui';
 
 import { Transfer } from 'ant-design-vue';
 
+const props = defineProps({
+  dataSource: {
+    type: Array as any,
+    default: () => [],
+  },
+  value: {
+    type: String,
+    default: '',
+  },
+});
+
 const emit = defineEmits(['confirm']);
 
-interface MockData {
-  key: string;
-  title: string;
-  description: string;
-  chosen: boolean;
-}
+// interface MockData {
+//   key: string;
+//   title: string;
+//   description: string;
+//   chosen: boolean;
+// }
 
-const data = ref();
+// const data = ref();
 
 const [Modal, modalApi] = useVbenModal({
   onCancel() {
@@ -34,38 +45,15 @@ const [Modal, modalApi] = useVbenModal({
     emit('confirm', targetKeysStr);
     modalApi.close();
   },
-  onOpenChange(isOpen: boolean) {
-    if (isOpen) {
-      data.value = modalApi.getData<Record<string, any>>();
-      // console.info('data', data.value);
-    }
-  },
+  // onOpenChange(isOpen: boolean) {
+  // if (isOpen) {
+  //   data.value = modalApi.getData<Record<string, any>>();
+  //   // console.info('data', data.value);
+  // }
+  // },
 });
-
-const mockData = ref<MockData[]>([]);
 
 const targetKeys = ref<string[]>([]);
-onMounted(() => {
-  getMock();
-});
-const getMock = () => {
-  const keys = [];
-  const mData = [];
-  for (let i = 0; i < 20; i++) {
-    const data = {
-      key: i.toString(),
-      title: `content${i + 1}`,
-      description: `description of content${i + 1}`,
-      chosen: Math.random() * 2 > 1,
-    };
-    if (data.chosen) {
-      keys.push(data.key);
-    }
-    mData.push(data);
-  }
-  mockData.value = mData;
-  targetKeys.value = keys;
-};
 </script>
 <template>
   <div>
@@ -73,7 +61,7 @@ const getMock = () => {
       <Transfer
         v-model:target-keys="targetKeys"
         :titles="['未选', '已选']"
-        :data-source="mockData"
+        :data-source="props.dataSource"
         show-search
         :list-style="{
           width: '350px',
--- apps/web-hoby/src/components/DynamicForm/modules/Transfer.vue
+++ apps/web-hoby/src/components/DynamicForm/modules/Transfer.vue
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-26 11:09:38
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-26 15:57:44
+ * @LastEditTime: 2025-03-28 22:35:02
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\components\DynamicForm\modules\Transfer.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,9 +13,21 @@ import { useVbenModal } from '@vben/common-ui';
 
 import { Input } from 'ant-design-vue';
 
-import Transfer from '../components/Transfer/Transfer.vue';
+import { mainGetCommonQueryData } from '#/api';
 
-const emit = defineEmits(['blur', 'change']);
+import Transfer from '../components/Transfer/Transfer.vue';
+import { convertToKeyTitleArray, parseQueryString } from './utils';
+
+const props = defineProps({
+  sourceData: {
+    type: Object as any,
+    default: () => {},
+  },
+  record: {
+    type: Object,
+    default: () => {},
+  },
+});
 
 const [Modal, modalApi] = useVbenModal({
   // 连接抽离的组件
@@ -26,18 +38,41 @@ const modelValue = defineModel<string>({
   default: () => '',
 });
 
-function onChange() {
-  emit('change', modelValue.value);
-}
-
 const transferRef = ref(null);
+const transferValue = ref({});
+
+const handleFocus = async () => {
+  // const record = containerApi.getValues();
+
+  const result = parseQueryString(props.sourceData.value, [
+    'table',
+    'condition',
+    'readFld',
+  ]);
+  const apiParams = {
+    INTERQUERYCON: result.condition,
+    INTERFLDNAMELIST: result.readFld,
+    INTERRESID: result.table,
+    ...props.record,
+  };
+  const { rs, data } = await mainGetCommonQueryData(apiParams);
+
+  if (rs === '1') {
+    transferValue.value = convertToKeyTitleArray(
+      data.dbRecsGrp,
+      result.readFld as string,
+      props.record[props.sourceData.fieldName] ?? '',
+    );
+  }
+};
 
 function open() {
+  handleFocus();
   modalApi
-    .setData({
-      content: modelValue.value,
-      payload: '外部传递的数据 payload',
-    })
+    // .setData({
+    //   content: modelValue.value,
+    //   payload: '外部传递的数据 payload',
+    // })
     .open();
 }
 
@@ -52,10 +87,12 @@ function onConfirm(targetKeys: string) {
       class="flex-1"
       allow-clear
       v-model:value="modelValue"
-      @blur="emit('blur')"
-      @change="onChange"
-      @click="open"
+      @focus="open"
     />
-    <Modal ref="transferRef" @confirm="onConfirm" />
+    <Modal
+      ref="transferRef"
+      :data-source="transferValue"
+      @confirm="onConfirm"
+    />
   </div>
 </template>
