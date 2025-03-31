6c00710d45f6486192a922c22d5920f5cfc980ee
Loong <wentloop@gmail.com>
2025-03-31 14:35:28 +0800

    feat: 新增文件上传和通用增删改查接口功能
    - 新增文件上传和通用增删改查接口功能。

--- apps/web-hoby/src/api/core/main.ts
+++ apps/web-hoby/src/api/core/main.ts
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-03 21:58:41
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-03-28 10:59:24
+ * @LastEditTime: 2025-03-31 14:32:28
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\api\core\main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -31,6 +31,57 @@ export async function mainGetDataApiCascade(data: any) {
  */
 export async function mainServiceApi(data: any) {
   return requestClient.post<any>('/buttonService', data, {
+    withCredentials: true,
+  });
+}
+
+/**
+ * 上传文件接口
+ */
+export async function uploadFile(data: any) {
+  return requestClient.post<any>('/uploadFile', data, {
+    withCredentials: true,
+  });
+}
+
+/**
+ * 上传文件接口
+ */
+export async function mainSendFile(data: any) {
+  return requestClient.post<any>('/buttonServiceFile', data, {
+    withCredentials: true,
+  });
+}
+
+/**
+ * 通用新增记录接口
+ */
+export async function mainAddRecrdApi(data: any) {
+  return requestClient.post<any>('/GenDataGrpAdd', data, {
+    withCredentials: true,
+  });
+}
+/**
+ * 通用删除记录接口
+ */
+export async function mainDeleteRecrdApi(data: any) {
+  return requestClient.post<any>('/GenDataGrpDelete', data, {
+    withCredentials: true,
+  });
+}
+/**
+ * 通用更新记录接口
+ */
+export async function mainUpdateRecrdApi(data: any) {
+  return requestClient.post<any>('/GenDataGrpUpdate', data, {
+    withCredentials: true,
+  });
+}
+/**
+ * 通用查询记录接口
+ */
+export async function mainSelectRecrdApi(data: any) {
+  return requestClient.post<any>('/GenDataGrpSelect', data, {
     withCredentials: true,
   });
 }
