44884f916ee1f6977f089ea89d2b6b570242f18c
Loong <wentloop@gmail.com>
2025-04-07 11:39:59 +0800

    feat: 启用页面表格操作列及默认功能按钮
    - 我的-企业信息页面
    - 我的-企业员工页面
    - 我的-操作账号页面

--- apps/web-hoby/src/views/my/basic/accounts.vue
+++ apps/web-hoby/src/views/my/basic/accounts.vue
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-26 17:15:52
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-04-01 18:51:58
+ * @LastEditTime: 2025-04-07 11:26:09
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\buyer\settlement.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -19,45 +19,45 @@ import CommonTable from '#/components/CommonTable/index.vue';
 const pageParams = ref({
   pageID: 'myCompanyEmployeeTellNoPage',
   showAddButton: false, // 控制是否显示新增按钮
-  onAdd: () => {
-    // 自定义新增处理逻辑
-    console.warn('新增');
-  },
+  // onAdd: () => {
+  //   // 自定义新增处理逻辑
+  //   console.warn('新增');
+  // },
 });
 // 定义表格列配置
 const columns = ref<ColumnDefinition[]>([
-  // {
-  //   title: '操作',
-  //   dataIndex: 'operation',
-  //   visible: true,
-  //   type: FieldType.OPERATION,
-  //   // defaultActions: ['view', 'edit', 'delete'],
-  //   defaultActions: false,
-  //   actionColumnProps: {
-  //     width: 200,
-  //     fixed: 'right',
-  //     align: 'center',
-  //   },
-  //   actions: [
-  //     {
-  //       text: '示例按钮',
-  //       type: 'link',
-  //       danger: true,
-  //       params: (record) => ({
-  //         pageID: 'myPrivateWareShopPage',
-  //         pageButtonID: 'warehouseShopClose',
-  //         actNo: record.actNo,
-  //         saleCmpName: record.saleCmpName,
-  //         wareName: record.wareName,
-  //       }),
-  //       disabled: (record) => record.onCateStatus !== '1',
-  //       successMsg: '停止营业成功',
-  //       errorMsg: '停止营业失败',
-  //       confirm: 'auto',
-  //       autoRefresh: true, // 默认为true，可省略
-  //     },
-  //   ],
-  // },
+  {
+    title: '操作',
+    dataIndex: 'operation',
+    visible: true,
+    type: 'operation',
+    defaultActions: ['view'],
+    // defaultActions: false,
+    actionColumnProps: {
+      width: 200,
+      fixed: 'right',
+      align: 'center',
+    },
+    actions: [
+      {
+        text: '示例按钮',
+        type: 'link',
+        danger: true,
+        visible: false, // 控制按钮是否显示
+        runMode: 'modal',
+        params: (record) => ({
+          pageID: '示例页面ID',
+          pageButtonID: '示例按钮ID',
+          ...record,
+        }),
+        disabled: (record) => record && false, // 控制按钮是否禁用
+        successMsg: '示例成功提示',
+        errorMsg: '示例失败提示',
+        confirm: 'auto',
+        autoRefresh: true, // 默认为true，可省略
+      },
+    ],
+  },
 ]);
 
 // 表格数据
--- apps/web-hoby/src/views/my/basic/employees.vue
+++ apps/web-hoby/src/views/my/basic/employees.vue
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-26 17:15:52
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-04-01 18:46:19
+ * @LastEditTime: 2025-04-07 10:23:34
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\views\buyer\settlement.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -18,46 +18,46 @@ import CommonTable from '#/components/CommonTable/index.vue';
 
 const pageParams = ref({
   pageID: 'myCompanyEmployeePage',
-  showAddButton: false, // 控制是否显示新增按钮
-  onAdd: () => {
-    // 自定义新增处理逻辑
-    console.warn('新增');
-  },
+  showAddButton: true, // 控制是否显示新增按钮
+  // onAdd: () => {
+  //   // 自定义新增处理逻辑
+  //   console.warn('新增');
+  // },
 });
 // 定义表格列配置
 const columns = ref<ColumnDefinition[]>([
-  // {
-  //   title: '操作',
-  //   dataIndex: 'operation',
-  //   visible: true,
-  //   type: FieldType.OPERATION,
-  //   // defaultActions: ['view', 'edit', 'delete'],
-  //   defaultActions: false,
-  //   actionColumnProps: {
-  //     width: 200,
-  //     fixed: 'right',
-  //     align: 'center',
-  //   },
-  //   actions: [
-  //     {
-  //       text: '示例按钮',
-  //       type: 'link',
-  //       danger: true,
-  //       params: (record) => ({
-  //         pageID: 'myPrivateWareShopPage',
-  //         pageButtonID: 'warehouseShopClose',
-  //         actNo: record.actNo,
-  //         saleCmpName: record.saleCmpName,
-  //         wareName: record.wareName,
-  //       }),
-  //       disabled: (record) => record.onCateStatus !== '1',
-  //       successMsg: '停止营业成功',
-  //       errorMsg: '停止营业失败',
-  //       confirm: 'auto',
-  //       autoRefresh: true, // 默认为true，可省略
-  //     },
-  //   ],
-  // },
+  {
+    title: '操作',
+    dataIndex: 'action',
+    visible: true,
+    type: 'operation',
+    defaultActions: ['view', 'edit', 'delete'],
+    // defaultActions: false,
+    actionColumnProps: {
+      width: 200,
+      fixed: 'right',
+      align: 'center',
+    },
+    actions: [
+      {
+        text: '示例按钮',
+        type: 'link',
+        danger: true,
+        visible: false, // 控制按钮是否显示
+        runMode: 'modal',
+        params: (record) => ({
+          pageID: '示例页面ID',
+          pageButtonID: '示例按钮ID',
+          ...record,
+        }),
+        disabled: (record) => record && false, // 控制按钮是否禁用
+        successMsg: '示例成功提示',
+        errorMsg: '示例失败提示',
+        confirm: 'auto',
+        autoRefresh: true, // 默认为true，可省略
+      },
+    ],
+  },
 ]);
 
 // 表格数据
