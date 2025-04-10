b656579a971b55f64e47772f2187c972e2826bef
Loong <wentloop@gmail.com>
2025-04-10 14:10:58 +0800

    fix: 更新密码输入组件v-model绑定语法
    - 更新密码输入组件的v-model绑定语法。

--- apps/web-hoby/src/components/DynamicForm/modules/passwordEncBypk.vue
+++ apps/web-hoby/src/components/DynamicForm/modules/passwordEncBypk.vue
@@ -2,7 +2,7 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-04-09 14:14:59
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-04-09 20:48:47
+ * @LastEditTime: 2025-04-10 13:39:01
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\components\DynamicForm\modules\passwordEncBypk.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -59,7 +59,7 @@ function processCipherText(cipherText: string): string {
 <template>
   <div>
     <InputPassword
-      v-model:model-value="showModelValue"
+      v-model:value="showModelValue"
       show-count
       allow-clear
       @change="handChange"
