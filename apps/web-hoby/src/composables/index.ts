a77e9e94b8cc017a22d940e6e9f2faaf8f663874
Loong <wentloop@gmail.com>
2025-04-15 17:15:24 +0800

    refactor: 重构组合式函数模块结构，修复表单组件未注册
    - 删除表单组合式函数导出文件
    - 重构组合式函数模块结构，将表单和表格相关功能拆分为更细粒度的子模块导出。
    - 删除表格相关的组合式函数导出文件

--- apps/web-hoby/src/composables/index.ts
+++ apps/web-hoby/src/composables/index.ts
@@ -2,14 +2,19 @@
  * @Author: Loong wentloop@gmail.com
  * @Date: 2025-03-18 11:26:16
  * @LastEditors: Loong wentloop@gmail.com
- * @LastEditTime: 2025-04-15 14:00:27
+ * @LastEditTime: 2025-04-15 17:14:28
  * @FilePath: \hoby-platform-client\apps\web-hoby\src\composables\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export * from './encrypt/encryption';
 export * from './enum/useEnums';
-export * from './form';
-export * from './table';
+export * from './form/useApiSelectProps';
+export * from './form/useMacroValue';
+export * from './form/useSetFieldList';
+export * from './form/useSetFieldRealValue';
+export * from './table/useSetButtons';
+export * from './table/useSetSchema';
+export * from './table/useTabs';
 export * from './useDynamicRoutes';
 export * from './useMainGetData';
 export * from './useParseResponse';
