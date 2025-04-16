c89d1b703bad22e24d958812bac82a4187d837c9
llx <1581304540@qq.com>
2025-04-16 16:46:55 +0800

    refactor: 不允许修改的input输入框readonly属性改为用disabled。

--- apps/web-hoby/src/views/products/components/EditSpecStandard.vue
+++ apps/web-hoby/src/views/products/components/EditSpecStandard.vue
@@ -412,7 +412,7 @@ defineExpose({
                     v-model="valueItem.specAttr"
                     class="min-w-0 flex-1 rounded-md border px-2 py-1 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                     :placeholder="`请输入${valueItem.specValue}`"
-                    :readonly="valueItem.isNew === 'false'"
+                    :disabled="valueItem.isNew === 'false'"
                     :class="{
                       'cursor-not-allowed bg-gray-100 focus:outline-none focus:ring-0':
                         valueItem.isNew === 'false',
